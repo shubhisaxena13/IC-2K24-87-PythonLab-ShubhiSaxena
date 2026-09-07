f56d5a4ac2c0f41c1287e6c6c85e604c7fa1b045
Shubhi Saxena <shubhisaxena3038@gmail.com>
2026-09-07 11:34:28 +0530

    Create arithmetic.py

---
+++ Lab1/arithmetic.py
@@ -0,0 +1,10 @@
+# This program performs basic arithmetic operations on two numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Quotient:", num1 / num2)
+print("Remainder:", num1 % num2)
