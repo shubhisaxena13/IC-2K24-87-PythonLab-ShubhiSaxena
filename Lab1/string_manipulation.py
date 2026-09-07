9aac5f2372d2e6663a4b562c95ddd164f08891ce
Shubhi Saxena <shubhisaxena3038@gmail.com>
2026-09-07 11:36:09 +0530

    Create string_manipulation.py

---
+++ Lab1/string_manipulation.py
@@ -0,0 +1,9 @@
+# This program performs different operations on a full name.
+
+name = input("Enter your full name: ")
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("Title Case:", name.title())
+print("Reversed:", name[::-1])
+print("Length:", len(name))
