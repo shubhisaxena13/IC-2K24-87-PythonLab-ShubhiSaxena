34e7139d6855294a690c817ce3a78dda85eb6c90
Shubhi Saxena <shubhisaxena3038@gmail.com>
2026-09-07 11:32:53 +0530

    Create greeting.py

---
+++ Lab1/greeting.py
@@ -0,0 +1,7 @@
+# This program takes the user's name, age and city and displays a greeting.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print(f"Hello {name}! You are {age} years old and live in {city}.")
