c33b5129a8efb15aa799b45e59286952194fa840
Shubhi Saxena <shubhisaxena3038@gmail.com>
2026-09-07 11:27:26 +0530

    Create variable_practice.py

---
+++ Lab1/variable_practice.py
@@ -0,0 +1,11 @@
+# This program demonstrates variables and their data types.
+
+name = "Shubhi"
+age = 20
+height = 5.4
+student = True
+
+print("Name:", name, type(name))
+print("Age:", age, type(age))
+print("Height:", height, type(height))
+print("Student:", student, type(student))
